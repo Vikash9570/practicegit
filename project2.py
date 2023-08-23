aac61aabf199cb47337b06836aeade7595052470
Vikash <vikashprd30@gmail.com>
2023-08-24 03:56:05 +0530

    commit project2

---
+++ project2.py
@@ -0,0 +1,23 @@
+class Student:
+    School_name="ggps"
+
+    def __init__(self,name,roll):
+        self.name=name
+        self.roll=roll
+        self.lap=self.Laptop
+
+    def show(self,m,n):
+        return (self.m+self.n)/2
+
+    class Laptop:
+
+        def show(self,cpu,ram):
+            print(self.ram,self.cpu)
+
+
+s1=Student("vikash",21)
+# s2=Student()
+lap1=Student.lap()
+lap1.show()
+s1.Student.show("i2",12)
+
